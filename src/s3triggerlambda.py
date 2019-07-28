9560c215cf3385fac0e3ca13b7aaf738a6373ab5
hdossani <hiren.dossani@gmail.com>
2019-07-28 12:22:00 -0400

    Updated for error handling

--- src/s3triggerlambda.py
+++ src/s3triggerlambda.py
@@ -1,49 +1,62 @@
 import boto3
 import json
 import os
+import re
 
 
 def lambda_handler(event, context):
     s3_resource = boto3.resource('s3')
-    comprehend = boto3.client(service_name='comprehend', region_name='us-east-2')
+    comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
     firehose_client = boto3.client('firehose',
                                    aws_access_key_id=os.environ['accessKey'],
                                    aws_secret_access_key=os.environ['secretAccessKey']
                                    )
-    rawbucket = s3_resource.Bucket('ckme136.twitter.raw')
+    rawbucket = s3_resource.Bucket('ckme136.capstone.twitter')
     if event:
         for file in rawbucket.objects.all():
-            print(file.key)
             rawdata = file.get()['Body'].read().decode('utf-8')
             tweets = rawdata.split("\n")
             for tweet in tweets:
                 try:
                     tweet_str = json.loads(tweet)
-                    tweet_json = json.loads(tweet_str)
-                    tweet_text = tweet_json["text"]
-                    tweet_lang = tweet_json["lang"]
-                    print(tweet_lang + "," + tweet_text)
+                    tweet_text = tweet_str.get("text", "Neutral Text")
+                    tweet_id = tweet_str.get("tweetid", -999)
+                    tweet_lang = "en"
+                    # clean up the text to remove username and RT
+                    tweet_text = re.sub(r"RT @[\w:]*", "", tweet_text)
+                    tweet_text = re.sub(r"@[\w]*", "", tweet_text)
+                    # remove links
+                    tweet_text = re.sub(r"(http|https)(.*)(?<!')(?<!\")", "", tweet_text)
+                    # remove emojis, symbols
+                    tweet_text = re.sub(r"[^\x00-\x7F]+", "", tweet_text)
+                    # print(tweet_text)
                     # Determine sentiment
                     sentiment_str = comprehend.detect_sentiment(Text=tweet_text, LanguageCode=tweet_lang)
+                    sentiment_json = dict(tweetid=tweet_id, text=tweet_text, sentiment=sentiment_str["Sentiment"],
+                                          positive=sentiment_str["SentimentScore"]["Positive"],
+                                          negative=sentiment_str["SentimentScore"]["Negative"],
+                                          neutral=sentiment_str["SentimentScore"]["Neutral"],
+                                          mixed=sentiment_str["SentimentScore"]["Mixed"])
+                    sentimentresponse = firehose_client.put_record(DeliveryStreamName='twitterSentimentStream',
+                                                                   Record={
+                                                                       'Data': json.dumps(sentiment_json) + '\n'
+                                                                   }
+                                                                   )
 
-                    sentiment_json = {"id": tweet_json["id"],
-                                      "text": tweet_json["text"],
-                                      "sentiment": sentiment_str["Sentiment"],
-                                      "positive": sentiment_str["SentimentScore"]["Positive"],
-                                      "negative": sentiment_str["SentimentScore"]["Negative"],
-                                      "neutral": sentiment_str["SentimentScore"]["Neutral"],
-                                      "mixed": sentiment_str["SentimentScore"]["Mixed"]}
-
-                    response = firehose_client.put_record(DeliveryStreamName='twitterSentimentStream',
-                                                          Record={
-                                                              'Data': json.dumps(sentiment_json) + '\n'
-                                                          }
-                                                          )
-                    # print(response)
                     # Determine entity
                     entity_str = comprehend.detect_entities(Text=tweet_text, LanguageCode=tweet_lang)
-                    print(json.dumps(entity_str))
-                except json.decoder.JSONDecodeError:
-                    print('Error')
-    # TODO implement
-    return "Hello World"
+                    for entity in entity_str["Entities"]:
+                        entity_json = {
+                            "tweetid": tweet_id,
+                            "entity": entity["Text"],
+                            "type": entity["Type"],
+                            "score": entity["Score"]}
+                        responseentity = firehose_client.put_record(DeliveryStreamName='twitterEntityStream',
+                                                                    Record={
+                                                                        'Data': json.dumps(entity_json) + '\n'
+                                                                    }
+                                                                    )
+                        print(tweet_text)
+                except json.decoder.JSONDecodeError as e:
+                    print('Error' + str(e))
+    return ""
--- src/twitterListener.py
+++ src/twitterListener.py
@@ -4,40 +4,49 @@ from tweepy.streaming import StreamListener
 import time
 import json
 import boto3
-
-cosumerAPIKey = 'cosumerAPIKey'
-consumerAPISecretKey = 'consumerAPISecretKey'
-apiAccessToken = 'apiAccessToken'
-apiAccessTokenSecret = 'apiAccessTokenSecret'
+import credentials
 
 
 class TwitterStreamListener(StreamListener):
     def __init__(self, time_limit=60):
         self.start_time = time.time()
         self.limit = time_limit
+        self.file = open("/Users/hirendossani/PycharmProjects/twitter_kinesis/src/resources/tweets.json", "w+")
         super(TwitterStreamListener, self).__init__()
 
     def on_data(self, data):
         if (time.time() - self.start_time) < self.limit:
+            try:
+                rawtweet = json.loads(data)
+                tweet_text = rawtweet.get("text", "Neutral Text")
+                tweet_id = rawtweet.get("id", -999)
 
-            json_data: str = json.dumps(data) + '\n'
+                json_data = {
+                            "tweetid": tweet_id,
+                            "text": tweet_text
+                             }
+                with open("/Users/hirendossani/PycharmProjects/twitter_kinesis/src/resources/tweets.json", "a") as tf:
+                    tf.write(json.dumps(json_data) + '\n')
 
-            firehose_client = boto3.client('firehose',
-                                           aws_access_key_id='aws_access_key_id',
-                                           aws_secret_access_key='aws_secret_access_key'
+                firehose_client = boto3.client('firehose',
+                                           aws_access_key_id=credentials.AWS_ACCESS_KEY_ID,
+                                           aws_secret_access_key=credentials.AWS_SECRET_ACCESS_KEY
                                            )
-            response = firehose_client.put_record(DeliveryStreamName='twitterDeliveryStream',
+                response = firehose_client.put_record(DeliveryStreamName='twitterDeliverStream',
                                                   Record={
-                                                      'Data': json_data
+                                                      'Data': json.dumps(json_data) + '\n'
                                                   }
                                                   )
-            print(response)
-            return True
+                print(rawtweet)
+                return True
+            except ValueError as e:
+                print("Error")
+                return True
         else:
             return False
 
 
-auth = OAuthHandler(cosumerAPIKey, consumerAPISecretKey)
-auth.set_access_token(apiAccessToken, apiAccessTokenSecret)
+auth = OAuthHandler(credentials.CONSUMERAPIKEY, credentials.CONSUMERAPISECRETKEY)
+auth.set_access_token(credentials.APIACCESSTOKEN, credentials.APIACCESSTOKENSECRET)
 twitterStream = Stream(auth, TwitterStreamListener())
-twitterStream.filter(languages=["en", "fr", "es"], track=["car", "food", "music", "sports"])
+twitterStream.filter(languages=["en"], track=['Republicans'])
